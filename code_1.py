16f3ba00f8fa0eeda5e04b38d753601c712e3677
PradeepNaik1 <cheyanoonsavath@gmail.com>
2022-08-06 12:35:54 +0530

    resaved

--- code_1.py
+++ code_1.py
@@ -1,1 +1,6 @@
-print("Hello World")
+# Printing range of numbers without using loops 
+def print_numbers(start , end ):
+    print(start)
+    return  print_numbers(start+1, end)  if end > start else print("End")
+        
+print_numbers(1, 12)
