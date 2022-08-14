5e515b45e75e663d92e9a2842a877c2eb14d1a42
PradeepNaik1 <91844217+PradeepNaik1@users.noreply.github.com>
2022-08-14 10:58:02 +0530

    graph code (for directed and undirected)

--- main.py
+++ main.py
@@ -1,53 +1,26 @@
-a = [
-    [0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 0],
-    [1, 1, 0, 0, 0],
-    [0, 1, 0, 1, 1],
-    [0, 0, 0, 0, 1]
-]
+class Graph:
+    def __init__(self, vertices):
+        self.__vertices = vertices
+        self.__adjacency_graph = [[0]*vertices for _ in range(vertices)]
 
-end_i, end_j = 4, 3
-
-path_so_far = []
-
-count = 0
-def go_to(i, j):
-    global path_so_far, end_j, end_i, a, count
-    if i < 0 or j < 0 or i > len(a)-1 or j > len(a[0])-1:
-        return
-    if (i,j) in path_so_far or a[i][j] > 0:
-        return
-    path_so_far.append((i,j))
-    a[i][j] = 2
-    if (i, j) == (end_i, end_j):
-        print("Found", path_so_far)
-        count+=1
-        path_so_far.pop()
-        return
-    else:
-
-        go_to(i + 1, j)
-        if count == 1:
-            return
-        go_to(i, j + 1)
-        if count == 1:
-            return
-        go_to(i, j - 1)
-        if count == 1:
-            return
-        go_to(i - 1, j)
-
-    # for i in range(5):
-    #     for j in range(5):
-    #         print(a[i][j], end=" ")
-    #     print('\n')
-    # print("------------------------")
-    return
+    def add_edge(self, v1, v2):
+        self.__adjacency_graph[v1][v2] = 1
+        self.__adjacency_graph[v2][v1] = 1
 
 
-go_to(0, 0)
+    def print_graph(self):
+        for row in self.__adjacency_graph:
+            for val in row:
+                print(val, end=" ")
+            print()
+    # For adding edge to directed graph the use this below commented method instead of add_edge()
+    # def add_edge(self, v1, v2):
+    #     self.__adjacency_graph[v1][v2] = 1
 
-for i in range(5):
-    for j in range(5):
-        print(a[i][j],end=" ")
-    print('\n')
+
+g = Graph(4)
+
+g.add_edge(1,1)
+g.add_edge(1,0)
+
+g.print_graph()
